905569701d135f684626e3f44d49385ecb13ba2b
Carlito Pedida Jr <carlped417@gmail.com>
2025-04-29 19:02:15 -0500

    env

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Link, NavLink } from "react-router";
+import { Link, NavLink, useLoaderData, useNavigate } from "react-router";
+import { logoutUser } from "~/appwrite/ auth";
 import { sidebarItems } from "~/constants";
 import { cn } from "~/lib/utils";
 
 const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
-  const user = {
-    name: "David Paige",
-    email: "d.paige@sample.com",
-    imageUrl: "/assets/images/david.webp"
+  const user = useLoaderData();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logoutUser();
+    navigate("/sign-in");
   };
 
   return (
@@ -44,20 +47,16 @@ const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
 
         <footer className="nav-footer">
           <img
-            src={user?.imageUrl || "/assets/images/david.webp"}
-            alt={user?.name || "David"}
+            src={user?.imageUrl}
+            alt={user?.name}
+            referrerPolicy="no-referrer"
           />
           <article>
             <h2>{user?.name}</h2>
             <p>{user?.email}</p>
           </article>
 
-          <button
-            onClick={() => {
-              console.log("logout");
-            }}
-            className="cursor-pointer"
-          >
+          <button onClick={handleLogout} className="cursor-pointer">
             <img
               src="/assets/icons/logout.svg"
               alt="logout"
