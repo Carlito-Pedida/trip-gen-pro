297943f9291ad0bdb644af58f5d4e8ad0293a117
Carlito Pedida Jr <carlped417@gmail.com>
2025-04-30 19:38:12 -0500

    trip detail info display

---
+++ app/appwrite/trips.ts
@@ -0,0 +1,37 @@
+import { Query } from "appwrite";
+import { totalmem } from "os";
+import { appwriteConfig, database } from "~/appwrite/client";
+
+export const getAllTrips = async (limit: number, offset: number) => {
+  const allTrips = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.tripsCollectionId[
+      (Query.limit(limit), Query.offset(offset), Query.orderDesc("createAt"))
+    ]
+  );
+
+  if (allTrips.total === 0) {
+    console.error("No trips found");
+    return { allTrips: [], total: 0 };
+  }
+
+  return {
+    allTrips: allTrips.documents,
+    total: allTrips.total
+  };
+};
+
+export const getTripById = async (tripId: string) => {
+  const trip = await database.getDocument(
+    appwriteConfig.databaseId,
+    appwriteConfig.tripsCollectionId,
+    tripId
+  );
+
+  if (!trip.$id) {
+    console.log("Trip not found");
+    return null;
+  }
+
+  return trip;
+};
